7e7ac3dae22709883304ee06667bb0540f77a574
Adam Sorrenti <adam.sorrenti@ryerson.ca>
2019-01-10 16:09:07 -0500

    Commit from IDEA

--- src/daily/coding/problem/Problem1.java
+++ src/daily/coding/problem/Problem1.java
@@ -4,7 +4,7 @@ package daily.coding.problem;
  *
  * @author Adam Sorrenti
  * @date 4-Jan-2019
- * @description Given a list of numbers and a number k, return whether any two 
+ * @description Given a list of numbers and a number k, return whether any two
  * numbers from the list add up to k. For example, given [10, 15, 3, 7] and k 
  * of 17, return true since 10 + 7 is 17.
  * 
