bd081ea17a0543b1c4cf1aab93107cb6c89107b2
Adam Sorrenti <adam.sorrenti@ryerson.ca>
2020-08-25 10:42:12 -0400

    Update and Problem #593

---
+++ Problem #593.py
@@ -0,0 +1,20 @@
+"""
+This problem was asked by Mailchimp.
+
+You are given an array representing the heights of neighboring buildings on a city street, from east to west. The city assessor would like you to write an algorithm that returns how many of these buildings have a view of the setting sun, in order to properly value the street.
+
+For example, given the array [3, 7, 8, 3, 6, 1], you should return 3, since the top floors of the buildings with heights 8, 6, and 1 all have an unobstructed view to the west.
+
+Can you do this using just one forward pass through the array?
+"""
+
+def sunSetView(list):
+    largest = list[len(list)-1]
+    count = 1
+    for i in range(len(list)-2,-1, -1):
+        if list[i] >= largest:
+            largest = list[i]
+            count += 1
+    return count
+
+print(sunSetView([3, 7, 8, 3, 9, 10]))
